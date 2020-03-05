3493c222d416f8e767ce9bfb4031e8611bce2924
Luísa Barros <qlullyq@gmail.com>
2020-03-05 17:37:21 -0300

    refactor: create list component

--- list.js
+++ list.js
@@ -1,19 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
 
 export default function List({getStorage}) {
   const [users, setUsers] = useState(getStorage || []);
-
+  console.log('USERS', users);
   useEffect(() => {
     (async () => {
-      // const storage = await AsyncStorage.getItem('users');
-      // users && setUsers(JSON.parse(storage));
-      console.log(getStorage);
+      getStorage !== null && setUsers(await getStorage);
     })();
-
-    console.log('LIST', users);
-  }, [getStorage, users]);
+  }, [getStorage]);
 
   return (
     <View>
