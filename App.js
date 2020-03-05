112f4ed5b4b29e6e924369b91ef2ce8ffced3228
Luísa Barros <qlullyq@gmail.com>
2020-03-05 17:39:25 -0300

    feat: useAsyncStorage hook

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import FormContainer from './form';
-import List from './list';
-import {useAsyncStorage} from './hook';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useCallback, useEffect} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
+import {useAsyncStorage} from '@react-native-community/async-storage';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +16,56 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  input: {
+    width: '100%',
+    padding: 15,
+    borderWidth: 1,
+    margin: 5,
+  },
 });
 
 const App = () => {
-  const [storageValue, updateStorage] = useAsyncStorage('users');
-  console.log('APP', storageValue);
+  const {getItem, setItem, removeItem} = useAsyncStorage('users');
+  const [users, setUsers] = useState([]);
+  const [newValue, setNewValue] = useState('');
+
+  const loadStorage = useCallback(async () => {
+    const item = JSON.parse(await getItem());
+    await setUsers(item === null ? [] : item);
+  }, [getItem]);
+
+  const handleSubmit = async () => {
+    await setItem(JSON.stringify([...users, newValue]));
+    setNewValue('');
+    loadStorage();
+  };
+
+  const handleReset = async () => {
+    try {
+      await removeItem();
+      loadStorage();
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    loadStorage();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <FormContainer setStorage={updateStorage} getStorage={storageValue} />
-      <List getStorage={storageValue} />
+      <>
+        <Button title="Resetar" onPress={handleReset} />
+        <TextInput
+          style={styles.input}
+          value={newValue}
+          onChangeText={text => setNewValue(text)}
+        />
+        <Button title="Enviar" onPress={handleSubmit} />
+        <View>
+          <Text>Listagem</Text>
+          {users && users.map(user => <Text key={user}>{user}</Text>)}
+        </View>
+      </>
     </SafeAreaView>
   );
 };
